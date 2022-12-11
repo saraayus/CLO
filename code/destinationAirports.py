0a834130e93086fdb6aa80c7db82b28f1b6d4e3d
Dmytro Vernyuk <102489410+Dvernyuk@users.noreply.github.com>
2022-12-11 05:12:16 +0100

    Add files via upload

---
+++ code/destinationAirports.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+import sys
+import re
+
+conf = SparkConf().setAppName('destinationAirports')
+sc = SparkContext(conf = conf)
+
+spark = SparkSession(sc)
+
+df = spark.read.option("header", "true").csv(sys.argv[1])
+
+df = df.groupBy("destinationAirport").count().sort("count", ascending=False)
+df.coalesce(1).write.csv(sys.argv[2])
+df.show()
