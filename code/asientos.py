faf3b0f6895ac8c9875ac2281468a24a53310dc6
Dmytro Vernyuk <102489410+Dvernyuk@users.noreply.github.com>
2022-12-12 20:16:51 +0100

    cambiada la relación y adaptado a cloud

--- code/asientos.py
+++ code/asientos.py
@@ -1,25 +1,25 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.sql.functions import avg, col, desc
-
+from pyspark.sql.functions import avg, col, desc, round
+import sys
 
 conf = SparkConf().setAppName('Asientos')
 sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
 
-path = "./itineraries.csv"
+path = sys.argv[1]
 
 #Devuelve un Dataframe con los datos del cs
 df = spark.read.option("header", "true").csv(path)
-df = df.select(df["seatsRemaining"].alias("REMAINING_SEATS").cast("int"), df["segmentsAirlineName"].alias("AIRLINE"))
+df = df.select(df["seatsRemaining"].alias("REMAINING_SEATS")\
+    .cast("int"), df["destinationAirport"])
 
 #Agrupamos según aerolineas
 #hacemos la media de valor de sitios vacios de cada grupo
-
-dfGroup = df.groupBy("AIRLINE") \
-    .agg(avg("REMAINING_SEATS").alias("AVG_REMAINING_SEATS")) \
+dfGroup = df.groupBy("destinationAirport") \
+    .agg(round(avg("REMAINING_SEATS"), 2).alias("AVG_REMAINING_SEATS")).sort("AVG_REMAINING_SEATS", ascending=False)
 
 #mostramos
 dfGroup.show()
-    
 
+dfGroup.coalesce(1).write.csv(sys.argv[2])
